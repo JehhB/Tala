f2463bcb5d9c4bc75ed561ff2af21562858c80a7
JehhB <jwy.bayod@gmail.com>
2022-06-04 20:31:47 +0800

    Added context for constellation data

--- src/pages/constellation-page.tsx
+++ src/pages/constellation-page.tsx
@@ -1,40 +1,60 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 
 import { useResponsiveSidebar } from "../hooks";
+import { ConstellationContext } from "../contexts";
 import { ReactComponent as MenuIcon } from "../assets/images/menu-icon.svg";
 import { ReactComponent as XMarkIcon } from "../assets/images/xmark-icon.svg";
+
 import HeaderBar from "../components/header-bar";
 import NavigationDrawer from "../components/navigation-drawer";
 
 import "./styles/page.css";
+import { Constellation } from "../utils";
 
 const ConstellationPage: FunctionComponent<{}> = function () {
   const [sidebarRef, isSidebarActive, toggleSidebar] = useResponsiveSidebar();
 
+  const [constellationData, setConstellationData] =
+    useState<Constellation | null>(null);
+
+  useEffect(function () {
+    fetch("/api/v1/constellation/test", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => setConstellationData(data));
+  }, []);
+
   return (
-    <div className="page">
-      <HeaderBar>
-        <div
-          className={"page__header__menu" + (isSidebarActive ? "--active" : "")}
-          onClick={function (event) {
-            event.stopPropagation();
-            toggleSidebar();
-          }}
-        >
-          {isSidebarActive ? <XMarkIcon /> : <MenuIcon />}
-        </div>
-      </HeaderBar>
-      <div className="page__main">
-        <div
-          ref={sidebarRef}
-          className={
-            "page__main__sidebar" + (isSidebarActive ? "--active" : "")
-          }
-        >
-          <NavigationDrawer />
+    <ConstellationContext.Provider
+      value={
+        constellationData ?? { id: "", categories: [], notes: [], links: [] }
+      }
+    >
+      <div className="page">
+        <HeaderBar>
+          <div
+            className={
+              "page__header__menu" + (isSidebarActive ? "--active" : "")
+            }
+            onClick={function (event) {
+              event.stopPropagation();
+              toggleSidebar();
+            }}
+          >
+            {isSidebarActive ? <XMarkIcon /> : <MenuIcon />}
+          </div>
+        </HeaderBar>
+        <div className="page__main">
+          <div
+            ref={sidebarRef}
+            className={
+              "page__main__sidebar" + (isSidebarActive ? "--active" : "")
+            }
+          >
+            <NavigationDrawer />
+          </div>
         </div>
       </div>
-    </div>
+    </ConstellationContext.Provider>
   );
 };
 
