4d8382f4e9dbb3db894363092489fca925494890
JehhB <jwy.bayod@gmail.com>
2022-06-06 22:12:56 +0800

    Modified mock notes template to be more extensive off all markdown syntax

--- mock/notes.ts
+++ mock/notes.ts
@@ -12,6 +12,65 @@ ${description}
 #### H4
 ##### H5
 ###### H6
+
+## Emphasis
+
+Emphasis, aka italics, with *asterisks* or _underscores_.
+
+Strong emphasis, aka bold, with **asterisks** or __underscores__.
+
+Combined emphasis with **asterisks and _underscores_**.
+
+Strikethrough uses two tildes. ~~Scratch this.~~
+
+## List
+
+1. First ordered list item
+2. Another item
+  * Unordered sub-list. 
+1. Actual numbers don't matter, just that it's a number
+  1. Ordered sub-list
+4. And another item.
+
+   You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
+
+   To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
+   Note that this line is separate, but within the same paragraph.⋅⋅
+   (This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
+
+## Links
+[I'm an inline-style link with title](https://www.google.com "Google's Homepage")
+
+## Images
+![alt text](https://via.placeholder.com/150)
+
+## Code
+
+Inline \`code\` has \`back-ticks around\` it.
+
+\`\`\`javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+\`\`\`
+
+## Tables
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+## Blockqoutes
+
+> Blockquotes are very handy in email to emulate reply text.
+> This line is part of the same quote.
+
+## Horizontal rule
+
+Three or more...
+
+---
 `;
 
   return constellation.notes.map(({ id, title, description, category_id }) => ({
