JehhB/Tala

commit 1e0a086bb9830c68f823f9ac5b5fc507ed17356f

Added notes in constellation page
Added notes in constellation page but cannot be accessed directly and category color is doesn't match with navigation drawer

src/components/constellation-note/constellation-note.tsx
@@ -1,0 +1,36 @@
+import { FunctionComponent, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+
+import { NoteContainer } from "../note-container";
+import { Note } from "../../utils";
+import { ConstellationContext } from "../../contexts";
+
+export const ConstellationNote: FunctionComponent<{}> = function () {
+  const [noteData, setNoteData] = useState<Note | null>(null);
+  const { categories } = useContext(ConstellationContext);
+
+  const { userName, constellationName, noteName } = useParams();
+
+  useEffect(
+    function () {
+      fetch(`/api/v1/${userName}/${constellationName}/${noteName}`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => setNoteData(data));
+    },
+    [userName, constellationName, noteName]
+  );
+
+  const color_percent =
+    (categories.find(
+      (category) => noteData && category.id === noteData.category_id
+    )?.index ?? 0) / categories.length;
+
+  return (
+    <NoteContainer title={noteData?.title ?? ""} color_percent={color_percent}>
+      <ReactMarkdown>{noteData?.content ?? ""}</ReactMarkdown>
+    </NoteContainer>
+  );
+};

src/App.tsx
@@ -6,20 +6,20 @@ import {
 } from "react-router-dom";
 
 import { ConstellationPage } from "./pages";
-import { NoteContainer } from "./components";
+import { ConstellationNote } from "./components";
 
 const App = function () {
   return (
     <Router>
       <Routes>
         <Route index element={<Navigate to="JohnDoe/Constellation" />} />
-        <Route path=":user">
+        <Route path=":userName">
           <Route index element={<Navigate to="./Constellation" />} />
-          <Route path=":constellation" element={<ConstellationPage />}>
+          <Route path=":constellationName" element={<ConstellationPage />}>
             <Route
               caseSensitive
-              path=":notes"
-              element={<NoteContainer title="test" />}
+              path=":noteName"
+              element={<ConstellationNote />}
             />
           </Route>
         </Route>

src/components/constellation-note/index.ts
@@ -1,0 +1,1 @@
+export * from "./constellation-note";

src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./constellation-graph";
+export * from "./constellation-note";
 export * from "./header-bar";
 export * from "./navigation-drawer";
 export * from "./note-container";

src/hooks/getRoute.ts
@@ -1,0 +1,2 @@
+
+* expr

commit 7cc955a2f55f64475883e5ed9b0526c77ab7bab4

Replaced fetch with fetch hook
Replaced fetch with fetch hook which fix bug introduced in 1e0a086 wherein note page would not load when navigated directly

src/components/constellation-note/constellation-note.tsx
@@ -1,39 +1,43 @@
-import { FunctionComponent, useContext, useEffect, useState } from "react";
+import { FunctionComponent, useContext } from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
+import useFetch from "react-fetch-hook";
 
 import { NoteContainer } from "../note-container";
 import { Note } from "../../utils";
 import { ConstellationContext } from "../../contexts";
 
 export const ConstellationNote: FunctionComponent<{}> = function () {
-  const [noteData, setNoteData] = useState<Note | null>(null);
-  const { categories } = useContext(ConstellationContext);
-
   const { userName, constellationName, noteName } = useParams();
-
-  useEffect(
-    function () {
-      fetch(`/api/v1/${userName}/${constellationName}/${noteName}`, {
-        method: "GET",
-      })
-        .then((response) => response.json())
-        .then((data) => setNoteData(data));
-    },
-    [userName, constellationName, noteName]
+  const note = useFetch<Note>(
+    `/api/v1/${userName}/${constellationName}/${noteName}`
   );
+  const constellation = useContext(ConstellationContext);
+
+  if (!constellation.data) return null;
+  const categories = constellation.data.categories;
 
   const level =
     (categories.find(
-      (category) => noteData && category.id === noteData.category_id
+      (category) => note.data && category.id === note.data.category_id
     )?.index ?? -1) + 1;
 
-  return (
-    <NoteContainer
-      title={noteData?.title ?? ""}
-      color_percent={level / categories.length}
-    >
-      <ReactMarkdown>{noteData?.content ?? ""}</ReactMarkdown>
-    </NoteContainer>
-  );
+  if (note.error) {
+    return (
+      <NoteContainer title="error">
+        <h1 style={{ color: "red" }}>note.error.message</h1>
+      </NoteContainer>
+    );
+  } else if (!note.isLoading && note.data) {
+    return (
+      <NoteContainer
+        title={note.data.title}
+        color_percent={level / categories.length}
+      >
+        <ReactMarkdown>{note.data.content}</ReactMarkdown>
+      </NoteContainer>
+    );
+  } else {
+    return null;
+  }
 };

src/components/navigation-drawer/navigation-drawer.tsx
@@ -13,51 +13,60 @@ import {
   groupBy,
   rgb,
   toValidTitle,
-  Constellation,
 } from "../../utils";
 
 import "./navigation-drawer.css";
 
 export const NavigationDrawer: FunctionComponent<{}> = function () {
-  const constellationData = useContext<Constellation>(ConstellationContext);
+  const constellation = useContext(ConstellationContext);
 
-  const notesData = groupBy(
-    constellationData.notes,
-    (element) => element.category_id
-  );
+  let categories = null;
 
-  const categoriesData = [...constellationData.categories].sort(
-    (a, b) => a.index - b.index
-  );
+  if (constellation.error) {
+    categories = (
+      <h1 style={{ color: "red" }}>{constellation.error.message}</h1>
+    );
+  } else if (!constellation.isLoading && constellation.data) {
+    const constellationData = constellation.data;
 
-  const categories = categoriesData.map((category, index) => (
-    <div key={index} className="category">
-      <div className="category__header">
-        <h2>{category.name}</h2>
+    const notesData = groupBy(
+      constellationData.notes,
+      (element) => element.category_id
+    );
+
+    const categoriesData = [...constellationData.categories].sort(
+      (a, b) => a.index - b.index
+    );
+
+    categories = categoriesData.map((category, index) => (
+      <div key={index} className="category">
+        <div className="category__header">
+          <h2>{category.name}</h2>
+        </div>
+        <nav
+          className="category__nav"
+          style={{
+            borderLeftColor: cssColor(
+              colorPalletGradient((index + 1) / categoriesData.length) ??
+                rgb(255, 0, 0)
+            ),
+          }}
+        >
+          {notesData[category.id].map((note, index) => (
+            <NavLink
+              key={index}
+              to={toValidTitle(note.title)}
+              className={({ isActive }) =>
+                "category__nav__link" + (isActive ? "--active" : "")
+              }
+            >
+              {note.title}
+            </NavLink>
+          ))}
+        </nav>
       </div>
-      <nav
-        className="category__nav"
-        style={{
-          borderLeftColor: cssColor(
-            colorPalletGradient((index + 1) / categoriesData.length) ??
-              rgb(255, 0, 0)
-          ),
-        }}
-      >
-        {notesData[category.id].map((note, index) => (
-          <NavLink
-            key={index}
-            to={toValidTitle(note.title)}
-            className={({ isActive }) =>
-              "category__nav__link" + (isActive ? "--active" : "")
-            }
-          >
-            {note.title}
-          </NavLink>
-        ))}
-      </nav>
-    </div>
-  ));
+    ));
+  }
 
   return (
     <aside className="navigation-drawer">

src/contexts/constellation-context.ts
@@ -1,10 +1,8 @@
 import { createContext } from "react";
+import { FetchResult } from "react-fetch-hook";
 
 import { Constellation } from "../utils";
 
-export const ConstellationContext = createContext<Constellation>({
-  id: "",
-  categories: [],
-  notes: [],
-  links: [],
+export const ConstellationContext = createContext<FetchResult<Constellation>>({
+  isLoading: true,
 });

src/pages/constellation-page/constellation-page.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent } from "react";
 import { Outlet, useParams } from "react-router-dom";
+import useFetch from "react-fetch-hook";
 
 import { useResponsiveSidebar } from "../../hooks";
 import { ConstellationContext } from "../../contexts";
@@ -17,26 +18,13 @@ import "./constellation-page.css";
 
 export const ConstellationPage: FunctionComponent<{}> = function () {
   const [sidebarRef, isSidebarActive, toggleSidebar] = useResponsiveSidebar();
-
-  const { user: userName, constellation: constellationName } = useParams();
-  const [constellationData, setConstellationData] =
-    useState<Constellation | null>(null);
-
-  useEffect(
-    function () {
-      fetch(`/api/v1/${userName}/${constellationName}`, { method: "GET" })
-        .then((response) => response.json())
-        .then((data) => setConstellationData(data));
-    },
-    [userName, constellationName]
+  const { userName, constellationName } = useParams();
+  const constellation = useFetch<Constellation>(
+    `/api/v1/${userName}/${constellationName}`
   );
 
   return (
-    <ConstellationContext.Provider
-      value={
-        constellationData ?? { id: "", categories: [], notes: [], links: [] }
-      }
-    >
+    <ConstellationContext.Provider value={constellation}>
       <div className="constellation-page">
         <HeaderBar>
           <div