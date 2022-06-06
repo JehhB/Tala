db44e3d0683056234a218e840fe4d67b0611d419
JehhB <jwy.bayod@gmail.com>
2022-06-06 23:41:15 +0800

    Styled headings, emphasis, lists, and links inside of a note container

--- mock/notes.ts
+++ mock/notes.ts
@@ -37,6 +37,12 @@ Strikethrough uses two tildes. ~~Scratch this.~~
    To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
    Note that this line is separate, but within the same paragraph.⋅⋅
    (This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
+1. a
+   1. b
+      1. c
+* a
+  * b
+    * c
 
 ## Links
 [I'm an inline-style link with title](https://www.google.com "Google's Homepage")
