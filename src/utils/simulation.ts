a4dadd219e268d0991a4051e511227cc41e53108
JehhB <jwy.bayod@gmail.com>
2022-06-13 08:58:48 +0800

    Added a centering force, spring force, and charge force

--- src/utils/simulation.ts
+++ src/utils/simulation.ts
@@ -1,13 +1,16 @@
 export type Point = {
-  x: number;
-  y: number;
-  vx: number;
-  vy: number;
-  fx: number;
-  fy: number;
+  x?: number;
+  y?: number;
+  vx?: number;
+  vy?: number;
+  fx?: number;
+  fy?: number;
+  index?: number;
 };
+
 export type ForceGenerator<Body extends Point> = (
   body: Body,
+  bodies: Body[],
   delta: number
 ) => {
   fx: number;
@@ -19,22 +22,33 @@ export class Simulation<Body extends Point> {
   readonly forceGenerators: ForceGenerator<Body>[];
 
   constructor(bodies: Body[], forceGenerators: ForceGenerator<Body>[]) {
-    this.bodies = bodies;
+    this.bodies = bodies.map(
+      (body, i) =>
+        ({
+          x: body.x ?? 0,
+          y: body.y ?? 0,
+          vx: body.vx ?? 0,
+          vy: body.vy ?? 0,
+          fx: body.fx ?? 0,
+          fy: body.fy ?? 0,
+          index: body.index ?? i,
+        } as Body)
+    );
     this.forceGenerators = forceGenerators;
   }
 
   advance(delta: number): void {
     this.bodies.forEach((body) => {
-      body.x += body.vx * delta;
-      body.y += body.vy * delta;
-      body.vx += body.fx * delta;
-      body.vy += body.fy * delta;
+      body.x! += body.vx! * delta;
+      body.y! += body.vy! * delta;
+      body.vx! += body.fx! * delta;
+      body.vy! += body.fy! * delta;
 
       body.fx = body.fy = 0;
       this.forceGenerators.forEach((forceGenerator) => {
-        const { fx, fy } = forceGenerator(body, delta);
-        body.fx += fx;
-        body.fy += fy;
+        const { fx, fy } = forceGenerator(body, this.bodies, delta);
+        body.fx! += fx;
+        body.fy! += fy;
       });
     });
   }
@@ -42,8 +56,95 @@ export class Simulation<Body extends Point> {
   activity(scale: number = 20): number {
     return (
       this.bodies
-        .map(({ fx, fy }) => Math.sqrt(fx * fx + fy * fy))
+        .map(({ fx, fy }) =>
+          Math.sqrt((fx! * fx! + fy! * fy!) / (scale * scale))
+        )
         .reduce((a, b) => a + b) / this.bodies.length
     );
   }
 }
+
+export const forceCenter = function (
+  origin: {
+    x: number;
+    y: number;
+  } = { x: 0, y: 0 }
+): ForceGenerator<Point> {
+  return function ({ x, y }: Point) {
+    const deltaX = x! - origin.x;
+    const deltaY = y! - origin.y;
+    return { fx: deltaX * deltaX, fy: deltaY * deltaY };
+  };
+};
+
+export const forceLink = function <Body extends Point>(
+  links: { source: string | number; target: string | number }[],
+  length: number,
+  minDeltaSq?: number,
+  hookeanSprintConstant: number = 1,
+  id: (body: Body) => string | number | null = (body) => body.index ?? null
+): ForceGenerator<Body> {
+  const mem = new Map<Body, Body[]>();
+  if (minDeltaSq === undefined) minDeltaSq = (length / 20) * (length / 20);
+
+  return (body, bodies) => {
+    const sources = links
+      .filter(({ target }) => target === id(body))
+      .map(({ source }) => source);
+
+    if (mem.get(body) === undefined) {
+      mem.set(
+        body,
+        bodies.filter(
+          (body) => sources.find((source) => source === id(body)) !== undefined
+        )
+      );
+    }
+
+    const { x: x0, y: y0 } = body;
+    const springForces = mem
+      .get(body)!
+      .map(({ x: x1, y: y1 }) => ({ x: x1! - x0!, y: y1! - y0! }))
+      .map(({ x, y }) => ({
+        delta: Math.sqrt(x * x + y * y) - length,
+        angle: Math.atan(y / x),
+      }))
+      .filter(({ delta }) => delta * delta > minDeltaSq!)
+      .map(({ delta, angle }) => ({
+        fx: delta * hookeanSprintConstant * Math.cos(angle),
+        fy: delta * hookeanSprintConstant * Math.sin(angle),
+      }));
+
+    return springForces.reduce((prev, curr) => ({
+      fx: prev.fx + curr.fx,
+      fy: prev.fy + curr.fy,
+    }));
+  };
+};
+
+export const forceCharges = function (
+  charge: number,
+  coulombConstant: number = 1
+): ForceGenerator<Point> {
+  return function ({ x: x0, y: y0 }, bodies) {
+    const forces = bodies
+      .map(({ x, y }) => ({ x: x! - x0!, y: y! - y0! }))
+      .map(({ x, y }) => ({
+        r: Math.sqrt(x * x + y * y),
+        angle: Math.tan(y / x),
+      }))
+      .map(({ r, angle }) => ({
+        f: (coulombConstant * charge * charge) / (r * r),
+        angle,
+      }))
+      .map(({ f, angle }) => ({
+        fx: f * Math.cos(angle),
+        fy: f * Math.sin(angle),
+      }));
+
+    return forces.reduce((prev, curr) => ({
+      fx: prev.fx + curr.fx,
+      fy: prev.fy + curr.fy,
+    }));
+  };
+};
