50574b06e5ecff06e350c0e985cfcb55e749d606
JehhB <jwy.bayod@gmail.com>
2022-06-22 15:54:11 +0800

    Added drag to network nodes

--- src/components/network-node/network-node.tsx
+++ src/components/network-node/network-node.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent, useContext, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
+import * as d3 from "d3";
 
 import { SimulationContext } from "../../contexts";
 
@@ -22,12 +23,51 @@ export const NetworkNode: FunctionComponent<NetworkNodeProps> = function ({
   color,
   to,
 }) {
+  const nodeRef = useRef<SVGGElement>(null);
   const simulation = useContext(SimulationContext);
   const node = simulation.nodes()[index];
   if (node === undefined) return null;
 
+  useEffect(
+    function () {
+      if (nodeRef.current === null) return;
+
+      const dragStarted = (event: any) => {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        node.fx = node.x;
+        node.fy = node.y;
+      };
+
+      const dragged = (event: any) => {
+        node.fx = event.x;
+        node.fy = event.y;
+      };
+
+      const dragEnded = (event: any) => {
+        if (!event.active) simulation.alphaTarget(0);
+        node.fx = null;
+        node.fy = null;
+      };
+
+      const nodeEl = d3
+        .select(nodeRef.current)
+        .call(
+          d3
+            .drag<SVGGElement, unknown>()
+            .on("start", dragStarted)
+            .on("drag", dragged)
+            .on("end", dragEnded)
+        );
+
+      return () => {
+        nodeEl.on("mousedown.drag", null);
+      };
+    },
+    [nodeRef, node, simulation]
+  );
+
   return (
-    <g transform={`translate(${node.x},${node.y})`}>
+    <g ref={nodeRef} transform={`translate(${node.x},${node.y})`}>
       <NavLink
         to={to}
         className={({ isActive }) =>
